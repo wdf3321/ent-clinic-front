e4e5640b31bb8265128aef8c42cb3bb20034fcd7
wdf3321 <wdf332@gmail.com>
2023-02-16 22:45:56 +0800

    12

--- quasar.config.js
+++ quasar.config.js
@@ -63,7 +63,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+      publicPath: '/',
       // analyze: true,
       env: {
         VITE_API: process.env.VITE_API
