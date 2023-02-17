277f87d2deb1f60f977b79ff7d42c1cc2c4cd4d5
wdf <wdf332@gmail.com>
2023-02-17 23:36:50 +0800

    edit

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -66,17 +66,9 @@
     </q-page-container>
     <div id="footer" class="column justify-center items-center text-center">
       <q-parallax src="~assets/parallax.jpg" :speed="0.75">
-<<<<<<< HEAD
+
         <div class="q-my-md-md q-my-xs text-h5">不知道如何預約嗎？</div>
         <div class="q-my-md-md q-my-xs text-h5">點擊下方按鈕，來加入會員吧！</div>
-=======
-        <h4 class="q-ma-md">
-          Lorem ipsum dolor sit,<br />
-          Lorem ipsum dolor sit,
-        </h4>
-
-        <h6 class="q-ma-md">02-94879487</h6>
->>>>>>> master
         <q-btn
           class="q-ma-lg bg-orange q-pa-md"
           to="/register"
