d14994120e0920c73a8f6c6a675ee909f8278c3c
wdf3321 <113666483+wdf3321@users.noreply.github.com>
2023-02-23 09:16:32 +0000

    Deploying to gh-pages from @ wdf3321/quasar2@77b9872ea91fe0e42f089518d5740a1c2e45d573 🚀

---
+++ assets/QCardActions.c3f46b4f.js
@@ -0,0 +1,1 @@
+import{j as t,aY as r,aZ as c,c as n,h as l,k as i}from"./index.259f3f0b.js";var d=t({name:"QCardActions",props:{...r,vertical:Boolean},setup(a,{slots:s}){const e=c(a),o=n(()=>`q-card__actions ${e.value} q-card__actions--${a.vertical===!0?"vert column":"horiz row"}`);return()=>l("div",{class:o.value},i(s.default))}});export{d as Q};
